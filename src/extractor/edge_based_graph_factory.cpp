fddeaf3515be37bceb85b9a06bdf9cd21f56bc77
torben <steinhardt@virtual-twins.com>
2025-03-04 17:25:18 +0100

    fixed level parameter

--- src/extractor/edge_based_graph_factory.cpp
+++ src/extractor/edge_based_graph_factory.cpp
@@ -436,7 +436,8 @@ EdgeBasedGraphFactory::GenerateEdgeExpandedNodes(const WayRestrictionMap &way_re
                                                         current_edge_source_coordinate_id,
                                                         current_edge_target_coordinate_id,
                                                         i,
-                                                        forward_data.flags.startpoint);
+                                                        forward_data.flags.startpoint,
+                                                        forward_data.flags.level);
 
                 current_edge_source_coordinate_id = current_edge_target_coordinate_id;
             }
