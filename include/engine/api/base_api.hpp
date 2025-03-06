c402de7338ada7b6cce628b41f198c93a90406b8
torben <steinhardt@virtual-twins.com>
2025-03-06 09:30:35 +0100

    Fixed syntax Switched makeWayPoint to NodeId Type

--- include/engine/api/base_api.hpp
+++ include/engine/api/base_api.hpp
@@ -56,21 +56,13 @@ class BaseAPI
             return std::string(
                 facade.GetNameForID(facade.GetNameIndex(phantom.forward_segment_id.id)));
         };
-        const auto toOsmId = [this](const auto &phantom) {
-                    return std::string(
-                        facade.GetOSMNodeIDOfNode(phantom.forward_segment_id.id);
-                };
+
         const auto noEmpty = [](const auto &name) { return !name.empty(); };
 
         // At an intersection we may have multiple phantom node candidates.
         // Combine them to represent the waypoint name.
         std::string waypoint_name = boost::algorithm::join(
             candidates | boost::adaptors::transformed(toName) | boost::adaptors::filtered(noEmpty),
-            INTERSECTION_DELIMITER);
-        // Do the same join logic for OSM Ids
-        // TODO check if that works
-        std::string waypoint_osm_id = boost::algorithm::join(
-            candidates | boost::adaptors::transformed(toOsmId) | boost::adaptors::filtered(noEmpty),
             INTERSECTION_DELIMITER);
 
         const auto &snapped_location = candidatesSnappedLocation(candidates);
@@ -89,7 +81,7 @@ class BaseAPI
                 snapped_location,
                 util::coordinate_calculation::greatCircleDistance(snapped_location, input_location),
                 waypoint_name,
-                waypoint_osm_id
+                candidates[0].forward_segment_id.id, // TODO check if that workaround is usable
                 {std::move(seg_hints)});
         }
         else
@@ -98,7 +90,7 @@ class BaseAPI
                 snapped_location,
                 util::coordinate_calculation::greatCircleDistance(snapped_location, input_location),
                 waypoint_name,
-                waypoint_osm_id);
+                candidates[0].forward_segment_id.id);
         }
     }
 
